0c91b890b03fd02f04313cbd2928728aee42f3ae
Marie Metay <mariemetay.pro@gmail.com>
2023-03-24 17:20:48 +0100

    Add data entity

--- app/src/main/java/com/cleanup/todoc/model/Task.java
+++ app/src/main/java/com/cleanup/todoc/model/Task.java
@@ -3,6 +3,10 @@ package com.cleanup.todoc.model;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
 
 import java.util.Comparator;
 
@@ -11,10 +15,12 @@ import java.util.Comparator;
  *
  * @author Gaëtan HERFRAY
  */
+@Entity(foreignKeys = @ForeignKey(entity = Project.class, parentColumns = "id", childColumns = "projectId"))
 public class Task {
     /**
      * The unique identifier of the task
      */
+    @PrimaryKey(autoGenerate = true)
     private long id;
 
     /**
